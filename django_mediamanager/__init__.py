4a1e7c95f9f81dce9e19277084806780128e39e5
Jason Beverage <jasonbeverage@gmail.com>
2016-05-09 13:54:03 -0400

    Changed import_module import to support latest Django

--- django_mediamanager/__init__.py
+++ django_mediamanager/__init__.py
@@ -8,7 +8,7 @@ def autodiscover():
     """
     # This code is based off django.contrib.admin.__init__
     from django.conf import settings
-    from django.utils.importlib import import_module
+    from importlib import import_module
     from django.utils.module_loading import module_has_submodule
 
     for app in settings.INSTALLED_APPS:
